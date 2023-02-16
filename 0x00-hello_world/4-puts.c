67b18a88eaea468e3223ee9543d15a72d05fd592
obetta1 <obettabenjamin44@gmail.com>
2023-02-16 10:33:00 -0800

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*this function C program that prints exactly 
+ *"Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
