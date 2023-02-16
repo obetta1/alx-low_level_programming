455f1aefbae19aaf502c251d9d643e45bb6a7216
obetta1 <obettabenjamin44@gmail.com>
2023-02-16 10:59:21 -0800

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*this main function  prints out a give string*/
+/**
+ * this main function  prints out a give string
+ *
+ * Return an int 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
