9c8a19671ae4f32adf4776c8621dcffc5f3ad275
obetta1 <obettabenjamin44@gmail.com>
2023-02-16 11:59:35 -0800

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints exactly a givein string,
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
