3ff4b260896e3364d8306491615a7cce14430923
obetta1 <obettabenjamin44@gmail.com>
2023-02-17 09:11:43 -0800

    core

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	int re;
 
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	/*n = rand() - RAND_MAX / 2;*/
 	re = n%10;
 	if (re > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, re);
