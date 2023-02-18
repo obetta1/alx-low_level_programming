1563192769f0720a05199c0159912bb6332acafc
obetta1 <obettabenjamin44@gmail.com>
2023-02-18 00:51:44 -0800

    optional task 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ int main(void)
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k <= 9; k++ )
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + 48);
 				putchar(j + 48);
 				putchar(k + 48);
-				if ((i * 10 + j * 10 + k) < 789)
+				if ((((i * 10 + j) * 10) + k) < 789)
 				{
 				putchar(',');
 				putchar(' ');
