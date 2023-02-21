0a2fff8f8882afdc6640f96fad17ccdbfee19579
obetta1 <obettabenjamin44@gmail.com>
2023-02-21 15:13:59 -0800

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,16 @@
 /**
  * main - prints _putchar, followed by a new line.
  *
- * Returns: 0
+ * Return: 0
  */
 
 int main(void)
 {
 	char str[] = "_putchar";
+
 	int i;
-	for (i = 0; i < 9; i++)
+
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(str[i]);
 	}
