5473a8246490ba62a640c1b85ab4f9fe72dd4b16
obetta1 <obettabenjamin44@gmail.com>
2023-02-21 14:11:39 -0800

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Returns: 0
+ */
+
+int main()
+{
+	char str[] = "_putchar";
+	int i;
+	for(i = 0; i < 9; i++ )
+	{
+	       	_putchar(str[i])
+	}
+  return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ c programming functions and nexted loop
