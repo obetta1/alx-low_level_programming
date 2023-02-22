2ea279e96ccc00e627a4f7a955029e772e6330b7
obetta1 <obettabenjamin44@gmail.com>
2023-02-22 02:54:55 -0800

    absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _abs - prints the absolutee value of a  given number,
+ * followed by a new line.
+ * @c: The number
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	int n = abs(c);
+
+	printf("%d", n);
+	return (0);
+}
