e1d25f2ce0bc67ae2420dfb6a1d32becdd755de3
obetta1 <obettabenjamin44@gmail.com>
2023-02-21 18:22:48 -0800

    print natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower - prints 10 times the alphabet, in lowercase,
+ * print_to_98 - prints 10 times the alphabet, in lowercase,
  * followed by a new line.
  * @n: number
  * Return: 0
