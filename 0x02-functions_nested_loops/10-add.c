35b6dd5158220c5468ad7cd9027efbe20ca528e8
obetta1 <obettabenjamin44@gmail.com>
2023-02-21 18:19:14 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * @a: number 1
+ * @b: number 2
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * @n: number
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
