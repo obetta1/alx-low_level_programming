3f71c462f9a7460316b3ed81679c22e175138581
obetta1 <obettabenjamin44@gmail.com>
2023-02-24 17:51:10 -0800

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * add - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * @a: number 1
+ * @b: number 2
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * add - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * @a: number 1
+ * @b: number 2
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 59; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (mult);
+}
