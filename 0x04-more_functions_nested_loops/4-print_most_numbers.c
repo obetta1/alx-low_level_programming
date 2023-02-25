c1f83ccf6cb523e96385126d3f10b8908caaedd1
obetta1 <obettabenjamin44@gmail.com>
2023-02-24 18:41:24 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_numbers - prints 10 times the alphabet, in lowercase,
+ * print_most_numbers - prints the numbers, from 0 to 9, excluding 2 and 4
  * followed by a new line.
  * Return: 0
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
