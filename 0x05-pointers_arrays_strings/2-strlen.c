db0e7b58a72fc28853a75a7188e4b5a94b0792c6
obetta1 <obettabenjamin44@gmail.com>
2023-02-27 12:35:11 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlrn - returns the lenght on string
+ * @s: string
+ *
+ * Returns: the lenght of the strung
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - print strin
+ * @str: string
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
