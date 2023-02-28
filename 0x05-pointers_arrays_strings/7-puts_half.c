0eec0b674edfa6481035a8a01375562904d2edcc
obetta1 <obettabenjamin44@gmail.com>
2023-02-27 16:31:51 -0800

    t

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,9 @@ void puts_half(char *str)
 	int i, len;
 
 	len = strlen(str);
-	for (i = (len - 1) / 2;  i < len; i++)
+	for (i = len / 2;  i < len; i++)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
 	_putchar('\n');
 }
