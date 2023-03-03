863295c1e09c534f9455493c83fa48252edd9bc3
obetta1 <obettabenjamin44@gmail.com>
2023-03-03 12:14:01 +0000

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * leet -  function that copies a string.
+ * @a: array
+ * Return: the pointer is the dest string
+ */
+char *leet(char *a)
+{
+	int k;
+}
+	for (k = 0; k < n; k++)
+	{
+		if (a[k] == 'a' || a[k] == 'A')
+			a[k] = '4';
+		else if (a[k] == 'e' || a[k] == 'E')
+			a[k] = '3';
+		else if (a[k] == 'o' || a[k] == 'O')
+			a[k] = '0';
+		else if (a[k] == 't' || a[k] == 'T')
+			a[k] = '7;
+		else if (a[k] == 'l' || a[k] == 'L')
+			a[k] = '1';
+		else
+	}
+	return (a);
+}
