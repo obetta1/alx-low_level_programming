47dd0629ddf65ede480fc6c381092403c8533fd8
obetta1 <obettabenjamin44@gmail.com>
2023-03-06 10:52:51 -0800

    more pointer and string

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset -  function that copies a string.
+ * @s: source string
+ * @b: constant byte
+ * @n: n byte
+ * Return: the pointer is the string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char c = memset(s, b, n);
+
+	return (c);
+}
