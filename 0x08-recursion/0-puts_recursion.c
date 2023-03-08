56168ae6a5c38c33fa1b19484efb9eef73a08150
obetta1 <obettabenjamin44@gmail.com>
2023-03-08 08:19:24 -0800

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - a function that prints a string,
+ * followed by a new line.
+ * @s: source string
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	puts("\n");
+}
