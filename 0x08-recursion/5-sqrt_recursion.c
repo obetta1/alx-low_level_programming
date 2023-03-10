d0450ed7af3cfbeaf18c4cc6141858c3c384d0de
obetta1 <obettabenjamin44@gmail.com>
2023-03-10 00:17:50 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns the
+ * natural square root of a number.
  * @n: number
  * Return: the square root of the number
  */
@@ -10,5 +11,29 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt(n));
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int root = _sqrt_recursion(n / 2);
+
+		if (root == -1)
+		{
+			return (-1);
+		}
+		else if ((root * root) > n)
+		{
+			return (-1);
+		}
+		else if (root * root == n)
+		{
+			return (root);
+		}
+		else
+		{
+			return (root + _sqrt_recursion(n - root * root));
+		}
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 int _putchar(char c);
+int wildcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
