45ec54401e7ee44d73d319a7cb77e78abaf61704
obetta1 <obettabenjamin44@gmail.com>
2023-03-10 15:10:53 -0800

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - funtion that print its name
+ * @argc: number of argument
+ * @argv: the array of string argument
+ * Return: alway 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
