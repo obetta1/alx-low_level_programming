dd202fea33995bba1983caa3622a8481b30cbf87
obetta1 <obettabenjamin44@gmail.com>
2023-03-17 15:59:46 -0700

    task1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: number of memory to allocate
+ * Return: pointer to the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(98);
+	}
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: string 1
+ * @s2: second string
+ * @n: n byte
+ * Return: new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, tr, sz, size1, size2, tot_size;
+
+	char *mo;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size1 = strlen(s1);
+	size2 = strlen(s2);
+	if (n >= size2)
+		sz = size2;
+	else
+		sz = n;
+	tot_size = size1 + size2 + 1;
+	mo = malloc(tot_size);
+	if (mo == NULL)
+	{
+		printf("Memory allocation failed\n");
+		return (NULL);
+	}
+	if (size1 <= size2)
+		tr = size2;
+	else
+		tr = size1;
+	for (i = 0; i < tr; i++)
+	{
+		if (i < size1)
+			mo[i] = s1[i];
+		if (i < sz)
+			mo[size1 + i] = s2[i];
+	}
+	return (mo);
+}
