c068dd7aca004ca418f1064b7d72fb2abc8b118e
obetta1 <obettabenjamin44@gmail.com>
2023-03-21 12:08:08 -0700

    updated the function prototype in the dog.h header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
+#include "dog.h"
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d:
