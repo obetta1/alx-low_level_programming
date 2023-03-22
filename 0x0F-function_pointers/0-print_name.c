d09d2bbbf1a51dbaa530920e0170dd00888b2cba
obetta1 <obettabenjamin44@gmail.com>
2023-03-22 06:21:15 -0700

    task1 a program that print  name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#incude "function_pointers.h"
+/**
+ * print_name - a function that prints a name.
+ * @name: string sorce
+ * @f: pointer to the function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		exit(EXIT_SUCCESS);
+	if ( f == NULL)
+		exit(EXIT_SUCCESS);
+	(*f)(name[i]);
+}
