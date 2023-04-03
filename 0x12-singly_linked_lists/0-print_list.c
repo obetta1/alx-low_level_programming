fe0a0a262f384c501cea53c153ea9b8ba8605d12
obetta1 <obettabenjamin44@gmail.com>
2023-04-03 05:41:28 -0700

    made corrction on 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,10 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nill)\n");
+			exit (EXIT_SUCCESS);
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h  = h->next;
 		res++;
