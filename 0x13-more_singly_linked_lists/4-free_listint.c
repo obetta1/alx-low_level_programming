455bb32e4ecaef4f8714e11248fcca12b88763d4
obetta1 <obettabenjamin44@gmail.com>
2023-04-04 12:07:51 -0700

    deleting a node at a given index

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node;
 
 	while (head)
 	{
-		new_node = head;
-		head = head->next;
-		free (new_node);
+		new_node = head->next;
+		free (head);
+		head = new_node;
 	}
 }
