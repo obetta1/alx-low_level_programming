be7d159a004eb654b7261d2a99281d8b462ef04c
obetta1 <obettabenjamin44@gmail.com>
2023-06-19 15:18:48 -0700

    bug fix on 5-frre_dog file

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  *free_dog - a function that frees dogs.
- @d: pointer to the dog_t struct
+ *@d: pointer to the dog_t struct
  */
 void free_dog(dog_t *d)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
