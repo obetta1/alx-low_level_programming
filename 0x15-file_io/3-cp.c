d41453b3104f0090d13827e0667873a55f6e05c9
obetta1 <obettabenjamin44@gmail.com>
2023-07-11 08:25:06 -0700

    updated file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,8 @@
  * @argc: number of arg
  * @argv: function args
  * Return: 0
-*/
+ * _dprintf - print to the STDERR_FILENO
+ */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, fr, fw;
@@ -19,39 +20,32 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		_dprintf("Error: Can't read from file", 98, argv[1]);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
-		exit(99);
-	}
+		_dprintf("Error: Can't write to", 99, argv[2]);
 	while ((fr = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		fw = write(file_to, buffer, fr);
 		if (fw != fr)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
-			exit(99);
-		}
+			_dprintf("Error: Can't write to", 99, argv[2]);
 	}
 	if (fr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
+		_dprintf("Error: Can't read from file", 98, argv[1]);
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
-		exit(100);
-	}
+		_dprintf("Error: Can't close", 100, argv[1]);
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
-		exit(100);
-	}
+		_dprintf("Error: Can't close", 100, argv[2]);
 	return (0);
+}
+/**
+ * _dprintf - print to the STDERR_FILENO
+ * @msg: error message
+ * @exit_code: exit code
+ * @arg: file name
+ */
+void _dprintf(char *msg, int exit_code, char *arg)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", msg, arg);
+	exit(exit_code);
 }
